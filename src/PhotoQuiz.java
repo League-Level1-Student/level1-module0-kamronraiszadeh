88d3f17e5c02be47c1bee46a633b2c888ff78e3e
kamronraiszadeh <league@WTS-iMac-11.attlocal.net>
2018-07-23 14:58:04 -0700

    photo quiz ip

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -3,7 +3,6 @@
  *    Level 1
  */
 
-
 import java.awt.Component;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,26 +18,33 @@ public class PhotoQuiz {
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
-                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
+		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This will make sure the program exits when you
+																	// close the window
 
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+		// 1. find an image on the internet, and put its URL in a String variable (from
+		// your browser, right click on the image, and select “Copy Image Address”)
+		String image = "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/guy-diamond-trolls-9.9.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component guy;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		guy = createImage(image);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(guy);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String i = JOptionPane.showInputDialog("What is the corlor of the guy's tie?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (i.equals("red")) {
+			System.out.println("correct");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
-		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+		else {
+			System.out.println("incorrect ");
+		}
+		// 9. remove the component from the quiz window (you may not see the effect of
+		// this until step 12)
+		quizWindow.remove(guy);
 		// 10. find another image and create it (might take more than one line of code)
 
 		// 11. add the second image to the quiz window
@@ -60,10 +66,6 @@ public class PhotoQuiz {
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	// *15. make something happen when mouse enters image
+	// (imageComponent.addMouseMotionListener())
 }
-
-
-
-
-
