033209317cc61b158b93ed1ca4fa48a4632bef8b
kamronraiszadeh <league@iMac.attlocal.net>
2018-08-25 11:25:50 -0700

    Cuteness tv ip

--- src/CutenessTv.java
+++ src/CutenessTv.java
@@ -1,20 +1,56 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class CutenessTv {
+public class CutenessTv implements ActionListener {
 public static void main(String[] args) {
+	new DuelingButtons().createUI();
+	}
 
-public void showButton() {
-    System.out.println("Button clicked");
-    JFrame frame= new JFrame();
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	JButton button = new JButton("You will get a fortune");
-	frame.add(button);
-	button.addActionListener(this);
-  frame.pack();
-	
-	
+	public void showButton() {
+	    System.out.println("Button clicked");
+	    JFrame frame= new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton a = new JButton("duck");
+		JButton b = new JButton ("frog") ;
+		JButton c = new JButton ("unicorn");
+		frame.add(b);
+		a.addActionListener(this);
+	  frame.pack();
+		
 }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("h6");
+	}
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	 
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+	     try {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+
 }
-}
+
